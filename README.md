ddf00932b52496f32c207686ae95f563cf2e16f6
0xMRTT <0xMRTT@tuta.io>
2022-10-04 22:17:18 +0200

    feat: intial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# RedCarpet
+
+RedCarpet GNOME info and send them to GNOME Team
+
+![In action](https://raw.githubusercontent.com/Atrophaneura/RedCarpet/main/assets/main_view.png)
+
+## Install 
+
+### AUR
+
+There is a package called `redCarpet-git` for RedCarpet available in the AUR
+
+### From source
+
+```
+gh repo cllone Atrophaneura/RedCarpet
+cd RedCarpet
+sudo meson builddir --prefix=/usr/local
+make install
+```
+
+## Contribute 
+
+See [CONTRIBUTING]
+
+### Translate 
+
+[![Translation status](https://hosted.weblate.org/widgets/atrophaneura/-/RedCarpet/open-graph.png)](https://hosted.weblate.org/engage/atrophaneura/)
+
+## About
+
+RedCarpet is a frontend of `gnome-info-collect` based on a mockup by Allan Day
---
+++ redcarpet/window.py
@@ -0,0 +1,43 @@
+# window.py
+#
+# Copyright 2022 Atrophaneura
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from ast import Gt
+from gi.repository import Gtk, Adw, Gio
+
+from .constants import rootdir, app_id
+
+
+@Gtk.Template(resource_path=f'{rootdir}/ui/window.ui')
+class RedCarpetWindow(Adw.ApplicationWindow):
+    __gtype_name__ = 'RedCarpetWindow'
+    
+    settings = Gio.Settings(app_id)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+    def save_window_props(self, *args):
+        win_size = self.get_default_size()
+
+        self.settings.set_int("window-width", win_size.width)
+        self.settings.set_int("window-height", win_size.height)
+
+        self.settings.set_boolean("window-maximized", self.is_maximized())
+        self.settings.set_boolean("window-fullscreen", self.is_fullscreen())
+
+    def show_preferences(self, *args):
+        pass
